48cc058e294a12d8e1d18a608164673eb4fe88b0
Surya-MERNstack <charusurya17@gmail.com>
2023-09-19 13:39:53 +0530

    add

--- routes/routes.js
+++ routes/routes.js
@@ -18,7 +18,9 @@ router.post("/register", async (req, res) => {
   const result = await UserData.findOne({ email: email });
 
   if (result)
-    return res.status(400).json({ message: "email is already exists!!!" , status : 400});
+    return res
+      .status(400)
+      .json({ message: "email is already exists!!!", status: 400 });
 
   try {
     await newUser.save();
@@ -30,34 +32,60 @@ router.post("/register", async (req, res) => {
   }
 });
 
+// router.post("/login", async (req, res) => {
+//   try {
+//     const result = await UserData.findOne({ email: req.body.email });
+
+//     if (!result)
+//       return res
+//         .status(401)
+//         .json({ message: "Email is not found", status: 401 });
+
+//     const isPasswordValid = await bcrypt.compare(
+//       req.body.password,
+//       result.password
+//     );
+
+//     if (!isPasswordValid)
+//       return res
+//         .status(402)
+//         .json({ message: "Password is wrong", status: 402 });
+
+//     res.status(200).json({ message: "Login Successfully", status: 200 });
+//   } catch (Err) {
+//     res.status(400).json({ message: "Server is error" });
+//   }
+// });
+
+
 router.post("/login", async (req, res) => {
-  
-
   try {
+    const { email, password } = req.body;
 
-    const result = await UserData.findOne({email: req.body.email});
+    // Find the user by email
+    const user = await UserData.findOne({ email });
 
-    if (!result) return res.status(401).json({ message: "Email is not found" ,status : 401});
+    if (!user) {
+      return res.status(401).json({ message: "Email is not found", status: 401 });
+    }
 
-    console.log("compare", result.email, result.password )
 
-    const validatePassword =  bcrypt.compare(
-     req.body.password , result.password
-    );
-  
-    if (!validatePassword)
-      return res.status(402).json({ message: "Password is wrong" ,message : 402});
-  
 
-    await UserData.findOne({
-      email: req.body.email,
-      password: req.body.password,
-    });
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(402).json({ message: "Password is wrong", status: 402 });
+    }
+
+    // Passwords match, login successful
     res.status(200).json({ message: "Login Successfully", status: 200 });
-  } catch (Err) {
-    res.status(400).json({ message: "Server is error" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error", status: 500 });
   }
 });
+
+
 
 router.get("/gold-rates", async (req, res) => {
   try {
